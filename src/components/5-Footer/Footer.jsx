c95c6845fe6df1dd0648d574890f3bbd117e4a44
Aya Elsherif <96848805+ayaelsherif21@users.noreply.github.com>
2025-04-24 06:20:03 +0200

    Added Footer

--- src/components/5-Footer/Footer.jsx
+++ src/components/5-Footer/Footer.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
 import './Footer.css'
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="flex">
+    
+      <ul className='flex'>
+        <li><a href="">About</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">Speaking</a></li>
+        <li><a href="">Uses</a></li>
+      </ul>
+      <p>© 2025 Spencer Sharp. All rights reserved.</p>
+    </footer>
+
   )
 }
