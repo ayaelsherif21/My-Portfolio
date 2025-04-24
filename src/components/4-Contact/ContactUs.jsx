8d257d47b336fe32f1ba037a055375564be9864a
Aya Elsherif <96848805+ayaelsherif21@users.noreply.github.com>
2025-04-24 06:19:24 +0200

    Added Contact Us section

--- src/components/4-Contact/ContactUs.jsx
+++ src/components/4-Contact/ContactUs.jsx
@@ -2,6 +2,29 @@ import React from 'react'
 import './ContactUs.css'
 export default function ContactUs() {
   return (
-    <div>ContactUs</div>
+    <div className="contact">
+        <h2 className='contactTitle'>
+        <span className='icon-envelope'></span>
+        Contact Us
+        </h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Odit, veritatis.</p>
+
+        <div className="flex ">
+          <form>
+          <div className='flex'>
+            <label htmlFor="email">Email Address:</label>
+            <input placeholder="Your Email.." required type="email" name="" id="email" />
+          </div>
+
+          <div className='flex' style={{marginTop:"1.2rem"}}>
+            <label htmlFor="message">Your Message: </label>
+            <textarea placeholder="Tell me anthing you want.." style={{ marginLeft:"2.75rem"}} required rows={5} cols={10}  name="" id="message"></textarea>
+          </div>
+             <button  className="submit ">Submit</button>
+           
+          </form>
+          <div className="animation"></div>
+        </div>
+    </div>
   )
 }
